9af0ab54dc5252a68686eef6dd1fce7c2d0ac8e1
Edilson Junior <edilsonrsjunior@gmail.com>
2022-10-14 13:37:51 -0300

    Add option to continue or exit

--- funcs.py
+++ funcs.py
@@ -9,4 +9,9 @@ def  clickChampion(characterPosX,characterPosY,btnX,btnY):
         pyautogui.click(characterPosX,characterPosY) #send click into specified position
         pyautogui.click(btnX,btnY)
         if keyboard.is_pressed("q"):
-            break
+            print("Stoping...")
+            print("Press y to continue or 0 to exit")
+            op = input("Insira a opção desejada!")	
+            if op == "0":
+                break   
+            
