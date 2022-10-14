e1821c24e89da116d7252cfd5107b7ac9c2aebb2
Edilson Junior <edilsonrsjunior@gmail.com>
2022-10-14 14:12:47 -0300

    fix error message not working

--- Picker.py
+++ Picker.py
@@ -70,8 +70,8 @@ while op != 0:
             print(f'X={characterPosX} e Y:{characterPosY}')
             print("\n\nCOMMANDS")
             print("Press Alt+S to Start")
-            print("Press Q to Stop")
-            keyboard.wait("Alt+s \n\n")
+            print("Press Q to Stop \n\n")
+            keyboard.wait("Alt+s")
             funcs.clickChampion(characterPosX,characterPosY,confirmBtnX,confirmBtnY)
         except KeyError:
             print("\n\n!!!!!!Agente ou botão não cadastrado, tente novamente!!!!!!!!\n\n\n")
