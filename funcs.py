7a37cb144ec926a6aad5b88d1b59a83e150c6136
Edilson Junior <edilsonrsjunior@gmail.com>
2022-10-14 14:04:46 -0300

    add correct continue or stop funciton

--- funcs.py
+++ funcs.py
@@ -1,20 +1,27 @@
 from concurrent.futures import thread
+from multiprocessing.connection import wait
+from time import sleep
 import pyautogui
 import keyboard
 
 pyautogui.PAUSE = 0.01 #Set low delay between click
 
 def  clickChampion(characterPosX,characterPosY,btnX,btnY):
-    while True:
+    out = False
+    while not out:
         pyautogui.click(characterPosX,characterPosY) #send click into specified position
         pyautogui.click(btnX,btnY)
         if keyboard.is_pressed("q"):
-            print("Stoping...")
-            print("Press y to continue or 0 to exit")
-            if(keyboard.is_pressed("y")):
-                print("Continuing...")
-                continue
-            if(keyboard.is_pressed("0")):
-                print("Exiting...")
-                break
+            print("\n\nStoping...\n")
+            print("Press y to continue or 0 to exit\n")
+            # wait keyboard press y or 0
+            while True:
+                if(keyboard.is_pressed("y")):
+                    print("Continuing...")
+                    break
+                if(keyboard.is_pressed("0")):
+                    print("Exiting...")
+                    out = True
+                    break
+            
             
